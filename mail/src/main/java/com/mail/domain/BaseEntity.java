400ee03cd700851a051d7c22f269e1139c07e51b
Thiago Bomfim <thiagobomfim1995@gmail.com>
2019-08-20 21:13:46 -0300

    Criação das entidades e do banco de dados

---
+++ mail/src/main/java/com/mail/domain/BaseEntity.java
@@ -0,0 +1,44 @@
+package com.mail.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public abstract class BaseEntity implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected Long codigo;
+
+    public void setCodigo(Long codigo) {
+        this.codigo = codigo;
+    }
+
+    public Long getCodigo() {
+        return codigo;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof BaseEntity)) return false;
+        BaseEntity that = (BaseEntity) o;
+        return Objects.equals(getCodigo(), that.getCodigo());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getCodigo());
+    }
+
+    @Override
+    public String toString() {
+        return this.getClass().getName() + " [ID=" + getCodigo() + "]";
+    }
+}
---
+++ mail/src/main/java/com/mail/enuns/EstadoEmail.java
@@ -0,0 +1,12 @@
+package com.mail.enuns;
+
+public enum EstadoEmail {
+
+    ENVIADO,
+    LIDO,
+    PENDENTE;
+
+
+    EstadoEmail() { }
+
+}
